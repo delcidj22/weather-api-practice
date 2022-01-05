943a1239384f693843dac95a15cdc965d6c7d629
delcidj22 <jdelcdid23@gmail.com>
2022-01-04 20:47:46 -0800

    add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: "eval-source-map",
-  devServer: {
-    contentBase: "./dist"
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
   },
-  plugins: [ 
+  plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'jsDevEnvironmentTemplate',
+      title: 'Shape Tracker',
       template: './src/index.html',
       inject: 'body'
-    })
+    }),
+    new Dotenv()
   ],
-  module: { 
+  module: {
     rules: [
       {
         test: /\.css$/,
@@ -32,25 +35,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
-      },
-      {
-        test: /\.(gif|png|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
-      },
-      {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
-      },
+      }
     ]
   }
 };
